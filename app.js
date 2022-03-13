1dcd241f7279b1b3ca7cc28405729ca77e30a1c0
Warren Fletcher <warren@fletch.band>
2022-03-13 12:27:37 +0100

    Testing Other Routes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Woodshed!');
 });
+app.get('/index', function (req, res) {
+  res.send('Test index!');
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
